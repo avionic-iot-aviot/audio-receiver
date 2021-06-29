2cfa2782098fa02255eef27700f0fed2aab1f93a
aviot <asantangelo90@gmail.com>
2021-06-29 16:16:31 +0200

    First functioning version.

--- main.py
+++ main.py
@@ -23,12 +23,14 @@ def GetIp(interface):
 # Create a UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
-server_address = (GetIp(config['GENERAL']['InterfaceRasp']), config['GENERAL']['PortRasp'])
+server_address = (GetIp(config['GENERAL']['InterfaceRasp']), int(config['GENERAL']['PortRasp']))
 s.bind(server_address)
 
 while True:
     print("####### Node is listening #######")
     data, address = s.recvfrom(8192)
+
+    print("ciao")
 
     packet = Packets.getPacketFromBytes(data)
     print (packet.printLitePacket())
@@ -37,6 +39,6 @@ while True:
     if (packet.Destination == GetIp(config['GENERAL']['InterfaceRasp'])):
         if(int(packet.Type) == 2):
             print("Data Received from: ", packet.Source)
-            f = open("./sample.txt", "wb")
+            f = open("./sample.txt", "ab")
             f.write(packet.Payload)
             f.close()
