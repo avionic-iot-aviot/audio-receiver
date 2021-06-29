a1ea1b97783e478cb8c23f93853193d2122722f0
Andrea Santangelo <asantangelo90@gmail.com>
2021-06-29 17:12:10 +0200

    Zero "sample.txt" every 100 packets received.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Packets import Packets
 from configparser import ConfigParser
 import ifaddr
 import socket
+import os
 
 
 config = ConfigParser()
@@ -20,17 +21,22 @@ def GetIp(interface):
     return temp
 
 
+# delete "sample.txt"
+if os.path.exists("./sample.txt"):
+    os.remove("./sample.txt")
+
+
 # Create a UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
 server_address = (GetIp(config['GENERAL']['InterfaceRasp']), int(config['GENERAL']['PortRasp']))
 s.bind(server_address)
 
+count=0
+
 while True:
     print("####### Node is listening #######")
     data, address = s.recvfrom(8192)
-
-    print("ciao")
 
     packet = Packets.getPacketFromBytes(data)
     print (packet.printLitePacket())
@@ -39,6 +45,11 @@ while True:
     if (packet.Destination == GetIp(config['GENERAL']['InterfaceRasp'])):
         if(int(packet.Type) == 2):
             print("Data Received from: ", packet.Source)
+            if count==100:
+                os.remove("./sample.txt")
+                count=0
             f = open("./sample.txt", "ab")
             f.write(packet.Payload)
             f.close()
+            count+=1
+            print("packet count: "+str(count))
