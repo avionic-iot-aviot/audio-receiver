322ab8c8a2b12ebb99241947dbe36fa1d50c9b46
Marco Cavalli <cmarcos46@gmail.com>
2021-09-24 13:52:17 +0200

    - Fixed audio-receiver writing more bytes than intended - Revert: We don't convert explicitly the bytes in int, but we let it do it by the python environment - Removed comments and prints unused

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = (GetIp(config['GENERAL']['InterfaceRasp']), int(config['GENERAL']['PortRasp']))
 s.bind(server_address)
 
-count=0
-
 while True:
     ##print("####### Node is listening #######")
     data, address = s.recvfrom(8192)
@@ -43,20 +41,23 @@ while True:
             bytes_effectively_written = 0
 
             i = 0
+            # The format of the bytes in the packet is as follow:
+            # 0 s1msb s1lsb 0 s2msb s2lsb 0 .... 0 sNmsb sNlsb
             while i < len(elements):
-                element_as_int = int.from_bytes(elements[i:i+1],"big", signed="True")
-                if element_as_int != 48:
+                if elements[i] != 48: # our separator is b'/x30' which is 48 in ASCII (literal '0')
                     temp_bytes.extend(elements[i:i+1])
-                elif element_as_int == 48:
+                elif elements[i] == 48:
                     if len(temp_bytes) == 2:
                         bytes_to_write.extend(temp_bytes[0:2])
                         bytes_effectively_written += 2
-                    elif len(temp_bytes) > 2:
-                        print("audio-receiver: I have more than 2 bytes")
                     temp_bytes = bytearray()
                 i += 1
             
             
+            # This is to recover the last sample "sNmsb sNlsb"
+            if len(temp_bytes) == 2:
+                    bytes_to_write.extend(temp_bytes[0:2])
+                    bytes_effectively_written += 2        
             
             if len(bytes_to_write) > 0:
                 # We don't write if we are not receiving (or 5 times neutral writing have been executed)  
@@ -65,7 +66,4 @@ while True:
                 f.write(bytes_to_write)
                 f.close()
             
-            print("audio-receiver: Data Received from: ", packet.Source, ' Size: ', len(elements), ' Effect: ', bytes_effectively_written)
-            
-            count+=1
-            ##print("audio-receiver: packet count "+str(count))
+            #print("audio-receiver: Data Received from: ", packet.Source, ' Size: ', len(elements), ' Effect: ', bytes_effectively_written)
